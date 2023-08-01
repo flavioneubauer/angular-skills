926b256a5fb1ffa046d8c1e0f2761dd995614c18
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-07-31 23:44:22 -0300

    Adding project

--- src/app/dashboard/card/card.component.ts
+++ src/app/dashboard/card/card.component.ts
@@ -1,24 +1,60 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  selector: "app-card",
+  templateUrl: "./card.component.html",
+  styleUrls: ["./card.component.css"],
 })
 export class CardComponent implements OnInit {
+  likes: number = 0;
 
   @Input() card;
-  constructor() { }
 
-  ngOnInit() {
-  }
+  buttonBlueFiveLimit: boolean = false;
+  buttonRedTenLimit: boolean = false;
+  isLoading: boolean = false;
 
-  onLike(card: any){
+  @Input() link: any = {
+    linkedinLink: "https://www.linkedin.com/in/marcosfelipesl/",
+  };
+
+  ngOnInit() {}
+
+  private apiUrl = "http://localhost:3000/api/counter";
+
+  constructor(private http: HttpClient) {}
+
+  @Output() loadingChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  onLike(card: number) {
     // TODO: incrementar o like, salvar via rest
+    this.likes++;
+    console.log(this.likes);
+    this.isLoading = true;
+    this.loadingChange.emit(true);
+
+    if (this.likes >= 5) {
+      this.buttonBlueFiveLimit = true;
+    }
+
+    if (this.likes >= 10) {
+      this.buttonRedTenLimit = true;
+    }
+
+    this.http
+      .post<any>(this.apiUrl, { value: this.likes })
+      .subscribe((data: string) => {
+        console.log("Adicionado " + JSON.stringify(data));
+        this.isLoading = false;
+        this.loadingChange.emit(false);
+      });
   }
 
-  onShare(card: any){
+  onShare(card: any) {
     // TODO: abrir o link do seu linkedin
+    if (this.link.linkedinLink) {
+      window.open(this.link.linkedinLink, "_blank");
+    }
   }
-
 }
---
+++ src/app/dashboard/loading-req/loading-req.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: "app-loading-req",
+  templateUrl: "./loading-req.component.html",
+  styleUrls: ["./loading-req.component.css"],
+})
+export class LoadingReqComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {}
+}
