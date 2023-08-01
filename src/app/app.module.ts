7ea304fc11c5c59b83a7e945bd83423709c04cf4
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-07-31 23:49:02 -0300

    Config Fonts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatButtonModule, MatCardModule, MatChipsModule } from '@angular/material';
+import { MatButtonModule, MatCardModule, MatChipsModule, MatProgressSpinnerModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
@@ -10,12 +10,27 @@ import { CardComponent } from './dashboard/card/card.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { FakeApiService } from './fake-api.service';
 import { HelloComponent } from './hello.component';
-
-
+import { LoadingReqComponent } from './dashboard/loading-req/loading-req.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule, MatButtonModule, MatChipsModule, MatCardModule, HttpClientModule, InMemoryWebApiModule.forRoot(FakeApiService, {delay: 5000}) ],
-  declarations: [ AppComponent, HelloComponent, DashboardComponent, CardComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatChipsModule,
+    MatCardModule,
+    HttpClientModule,
+    MatProgressSpinnerModule,
+    InMemoryWebApiModule.forRoot(FakeApiService, { delay: 5000 }),
+  ],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    DashboardComponent,
+    CardComponent,
+    LoadingReqComponent,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
